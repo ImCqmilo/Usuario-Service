fcbacbb0e626c8346da7dfb65a23133fe0b860df
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-05 18:05:40 -0500

    Primer Commit

---
+++ src/main/java/com/team2/usuarioservice/entity/AuthRequest.java
@@ -0,0 +1,4 @@
+package com.team2.usuarioservice.entity;
+
+public class AuthRequest {
+}
---
+++ src/test/java/com/team2/usuarioservice/UsuarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.usuarioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
