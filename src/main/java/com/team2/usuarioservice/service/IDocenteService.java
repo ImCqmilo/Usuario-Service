1e1ecebebb468b30e067c8a8151a3891262a260e
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-11 15:12:45 -0500

    Implementacion del metodo findByID y save en la clase IDocenteService

--- src/main/java/com/team2/usuarioservice/service/IDocenteService.java
+++ src/main/java/com/team2/usuarioservice/service/IDocenteService.java
@@ -1,4 +1,10 @@
 package com.team2.usuarioservice.service;
 
+import com.team2.usuarioservice.entity.Docente;
+
+import java.util.List;
+
 public interface IDocenteService {
+    Docente findById(Long docenteId);
+    Docente save(Docente docente);
 }
