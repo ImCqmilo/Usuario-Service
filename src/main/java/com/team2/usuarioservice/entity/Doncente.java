1d233b396842b5e94d034ac832bd7cd625bba1df
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-11 15:00:33 -0500

    Creacion de valores para la clase "Docente"

--- src/main/java/com/team2/usuarioservice/entity/Doncente.java
+++ src/main/java/com/team2/usuarioservice/entity/Doncente.java
@@ -1,4 +1,25 @@
 package com.team2.usuarioservice.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Docente")
 public class Doncente {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long docenteId;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    @Column(name = "correo_electronico", unique = true)
+    private String correo;
+
 }
+
