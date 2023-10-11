cb881660bbb34b73ceddb6d747e34478d213b922
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-11 15:07:14 -0500

    Extencion de CrudRepository en la clase IDocenteDao

--- src/main/java/com/team2/usuarioservice/Dao/IDocenteDao.java
+++ src/main/java/com/team2/usuarioservice/Dao/IDocenteDao.java
@@ -1,4 +1,7 @@
 package com.team2.usuarioservice.Dao;
 
-public interface IDocenteDao {
+import com.team2.usuarioservice.entity.Docente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IDocenteDao extends CrudRepository<Docente, Long> {
 }
