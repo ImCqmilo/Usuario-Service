39798b1682d596cfb0ed9cea3d1bad2879539de3
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-11 15:19:23 -0500

    Implementacion de los metodos findByID y save que se encuenran en la clase IDocenteService, en la clase DocenteImpl

--- src/main/java/com/team2/usuarioservice/service/DocenteImpl.java
+++ src/main/java/com/team2/usuarioservice/service/DocenteImpl.java
@@ -1,4 +1,17 @@
 package com.team2.usuarioservice.service;
 
-public class DocenteImpl {
+import com.team2.usuarioservice.Dao.IDocenteDao;
+import com.team2.usuarioservice.entity.Docente;
+
+public class DocenteImpl implements IDocenteService{
+    IDocenteDao docenteDao;
+    @Override
+    public Docente findById(Long docenteId) {
+        return docenteDao.findById(docenteId).get();
+    }
+
+    @Override
+    public Docente save(Docente docente) {
+        return docenteDao.save(docente);
+    }
 }
