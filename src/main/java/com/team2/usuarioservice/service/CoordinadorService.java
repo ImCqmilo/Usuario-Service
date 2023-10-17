dd0e5237a7b543898d868e6d91b4b6430c558695
David Mercado Tut <juan.mercado02@uceva.edu.co>
2023-10-17 13:08:32 -0500

    Crear el metodo de editar coordinador y se implementa

--- src/main/java/com/team2/usuarioservice/service/CoordinadorService.java
+++ src/main/java/com/team2/usuarioservice/service/CoordinadorService.java
@@ -2,13 +2,11 @@ package com.team2.usuarioservice.service;
 
 import com.team2.usuarioservice.dao.ICoordinadorDao;
 import com.team2.usuarioservice.entity.Coordinador;
-import com.team2.usuarioservice.repository.CoordinadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CoordinadorService implements ICoordinadorService {
@@ -30,4 +28,24 @@ public class CoordinadorService implements ICoordinadorService {
 
     @Override
     public Coordinador save(Coordinador coordinador){return CoordinadorDao.save(coordinador);}
+
+
+   public Coordinador editarCoordinador( Long id,
+                                        Long Nuevo_coordinadorId,
+                                        String Nuevo_nombre,
+                                        String Nuevo_apellido,
+                                        String Nuevo_telefono,
+                                        String Nuevo_correo){
+        Coordinador coordinador = CoordinadorDao.findById(id).get();
+        if( coordinador != null ){
+            coordinador.setCoordinadorId(Nuevo_coordinadorId);
+            coordinador.setNombre(Nuevo_nombre);
+            coordinador.setApellido(Nuevo_apellido);
+            coordinador.setTelefono(Nuevo_telefono);
+            coordinador.setCorreo(Nuevo_correo);
+        }
+        return coordinador;
+    }
+
 }
+
