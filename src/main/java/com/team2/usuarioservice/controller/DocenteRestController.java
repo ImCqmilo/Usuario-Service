909b1d44ce7adf20346192c53d52553af7f1dab1
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-17 10:50:03 -0500

    Agregacion de la etiqueta @Autowired, cambio del @RequestMaping()

--- src/main/java/com/team2/usuarioservice/controller/DocenteRestController.java
+++ src/main/java/com/team2/usuarioservice/controller/DocenteRestController.java
@@ -1,17 +1,16 @@
 package com.team2.usuarioservice.controller;
 
-import com.team2.usuarioservice.Dao.IDocenteDao;
 import com.team2.usuarioservice.entity.Docente;
-import com.team2.usuarioservice.service.IDocenteService;
+import com.team2.usuarioservice.service.DocenteImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/pais-service")
+@RequestMapping("/api/v1/user-service")
 public class DocenteRestController {
 
-    private IDocenteService docenteService;
-
+    @Autowired
+    private DocenteImpl docenteService;
     @PostMapping("/docente")
     public Docente crearDocente(@RequestBody Docente docente){
         return docenteService.save(docente);
