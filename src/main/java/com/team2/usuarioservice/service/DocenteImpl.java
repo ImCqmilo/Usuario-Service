22056790116c550d65f2534d94ffba28564ef54a
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-17 10:47:48 -0500

    Agregacion de la etiqueta @Service y @Autowired

--- src/main/java/com/team2/usuarioservice/service/DocenteImpl.java
+++ src/main/java/com/team2/usuarioservice/service/DocenteImpl.java
@@ -1,11 +1,16 @@
 package com.team2.usuarioservice.service;
 
-import com.team2.usuarioservice.Dao.IDocenteDao;
+import com.team2.usuarioservice.dao.IDocenteDao;
 import com.team2.usuarioservice.entity.Docente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+@Service
 public class DocenteImpl implements IDocenteService{
 
+    @Autowired
     IDocenteDao docenteDao;
     @Override
     public Docente findById(Long docenteId) {
