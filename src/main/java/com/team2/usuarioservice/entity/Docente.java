b26f490d55aaf4c179891bd6baad73fb8c8321ff
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-17 19:12:21 -0500

    eliminacion de validacion que tenia el correo

--- src/main/java/com/team2/usuarioservice/entity/Docente.java
+++ src/main/java/com/team2/usuarioservice/entity/Docente.java
@@ -16,7 +16,6 @@ public class Docente {
     private String nombre;
     private String apellido;
     private String telefono;
-    @Column(name = "correo_electronico", unique = true)
     private String correo;
 
 }
