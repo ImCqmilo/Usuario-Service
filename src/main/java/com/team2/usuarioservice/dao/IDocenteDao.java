ed2460f08b52c570ccdc1b5d9859fbbadaad9868
Jcamiloag <juan.arevalo01@uceva.edu.co>
2023-10-18 18:01:34 -0500

    Cambio de nombre del package DAO

--- src/main/java/com/team2/usuarioservice/dao/IDocenteDao.java
+++ src/main/java/com/team2/usuarioservice/dao/IDocenteDao.java
@@ -1,4 +1,4 @@
-package com.team2.usuarioservice.Dao;
+package com.team2.usuarioservice.dao;
 
 import com.team2.usuarioservice.entity.Docente;
 import org.springframework.data.repository.CrudRepository;
