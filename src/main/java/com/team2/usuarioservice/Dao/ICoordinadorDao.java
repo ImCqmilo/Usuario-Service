6764c7c38d1411acbfe3ddd576a73827ee7b91cf
Juan Camilo Cardona <juan.cardona09@uceva.edu.co>
2023-10-19 13:18:31 -0500

    Arreglo de conlfico en la carpeta dao

--- src/main/java/com/team2/usuarioservice/Dao/ICoordinadorDao.java
+++
@@ -1,7 +0,0 @@
-package com.team2.usuarioservice.Dao;
-
-import com.team2.usuarioservice.entity.Coordinador;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ICoordinadorDao extends CrudRepository<Coordinador, Long> {
-}
